a5521db2948ca7e163397638bf62635595b6479d
Alexandru Ionescu <33354331+al3xionescu@users.noreply.github.com>
2019-03-04 21:33:50 +0200

    Create 392. Is Subsequence.cpp

---
+++ 392. Is Subsequence.cpp
@@ -0,0 +1,30 @@
+/*
+Given a string s and a string t, check if s is subsequence of t.
+
+You may assume that there is only lower case English letters in both s and t. t is potentially a very long (length ~= 500,000) string, and s is a short string (<=100).
+
+A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+s = "abc", t = "ahbgdc"
+
+Return true.
+
+Example 2:
+s = "axc", t = "ahbgdc"
+
+Return false.
+*/
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        int j = 0;       
+        for (int i=0; i<n&&j<m; i++)
+            if (s[j] == t[i])
+                j++;
+        return (j==m);
+    }
+};
